d066bee969e746f71d6d502c408b13e906f96f33
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-16 10:37:45 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 132453
+let accountNmae = "kashish jat"
+var accountEmail = "kashish00@gmail"
+
+accountCity = "jaipur"
+// console.log(accountId);
+console.table([accountId,accountNmae,accountEmail,accountCity]);
