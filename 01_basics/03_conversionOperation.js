228b481610ed0a758588b4e039fc59bf5031173b
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-16 12:21:13 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = 45
+
+console.log(typeof score)
+
+// 45 = 45
+// 45abc = NaN (not a number)
+// true = 1
+//false = 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 29
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
