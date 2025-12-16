cf73e7803b20d8dd12fd2abe733d0aedf6fdfc3d
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-16 11:56:45 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // it is used for strict version of js
+
+console.log(3+3)
+
+console.log("kashish")
+
+let name="kashuuu"
+let age = 21
+let isLOggedin= false
+
+//number = 2 to the power of 53
+// bigint = for large number
+//string = ""
+//bolean = true/false
+//null = standalone value
+//undefined = valued not assigned
+// symbol= unique
+
+console.log(typeof age)
+
+console.log(typeof null)
