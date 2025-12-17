1ce1a29b5355aa3a7138ddd88c3fcdf9df5bb998
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-17 14:16:19 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+const name = "kashish";
+const repositoryCount = 50;
+
+console.log(`Hello my name is ${name} and my repo count is ${repositoryCount}`);
+
+
+const gameName = new String("Chess");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('e'));
+
+
+// const newString = gameName.substring(0,3);
+// const anotherString = gameName.slice(-3,4);
+
+// console.log(newString);
+// console.log(anotherString);
+
+const newStringOne = "     kashish     ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+// console.log(newStringOne.replace());
+
+const url="https://kashish.com/kashishh&20jatt";
+console.log(url.replace('&20', '-'));
+
