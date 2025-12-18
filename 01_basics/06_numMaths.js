0750edf9ed1979e2cf38423b686c5286adf8ba60
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-18 09:50:07 +0000

    number & maths

---
+++ 01_basics/06_numMaths.js
@@ -0,0 +1,44 @@
+const score =400;
+// console.log(score);
+
+const score1 = new Number(500);
+// console.log(score1);
+
+// console.log(score1.toString().length);
+// console.log(score1.toFixed(3));
+// console.log(score1.toExponential());
+
+
+const num = 12.34567;
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(3));
+
+const score2 = 10000000;
+// console.log(score2.toLocaleString('en-IN'));
+
+
+
+//++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.SQRT2);
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.trunc(4.7));
+
+// console.log(Math.pow(2,3));
+// console.log(2**3);
+
+// console.log(Math.sqrt(64));
+
+// console.log(Math.abs(-55));
+// console.log(Math.floor(Math.random()*10)+1); 
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
