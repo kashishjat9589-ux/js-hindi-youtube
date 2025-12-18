4d9e838e1a93144d5e7cf8a40bc69f643e6ecbf1
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-18 11:53:44 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+// ARRAYS
+
+const arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// console.log(arr[3]);
+
+// ARRAY METHODS
+
+// arr.push(8)
+// arr.push(6)
+// arr.pop()
+
+
+// arr.unshift(29)
+// arr.shift()
+
+
+// console.log(arr.includes(3));
+// console.log(arr.indexOf(4));
+
+const newArray = arr.join()
+// console.log(newArray);
+// console.log(arr);
+
+
+// SLICE AND SPLICE METHODS
+
+console.log("A " , arr);
+
+const myArray = arr.slice(1,4)
+
+console.log(myArray);
+
+console.log("B " , arr);
+
+const myArray1 = arr.splice(0,3)
+console.log("C " , arr);
+console.log(myArray1);
+
