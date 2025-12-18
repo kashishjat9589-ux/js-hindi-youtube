441c50f81813dd4970d9f651b12503faabccea7c
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-18 10:45:32 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,32 @@
+// Date
+
+let myDate = new Date()
+// console.log(myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+
+let specificDate = new Date('2020-01-01')
+// console.log(specificDate.toString());
+
+
+
+
+// ++++++++++++++++++++TIME+++++++++++++++++++++++++++++++
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+// console.log(specificDate.getTime());
+// console.log(specificDate.getFullYear());
+// console.log(specificDate.getMonth());
+// console.log(specificDate.getDate());
+// console.log(specificDate.getDay());
+// console.log(specificDate.getHours());
+// console.log(specificDate.getMinutes());
+// console.log(specificDate.getSeconds());
