12d9194b9b80a7c24c61db69d9529df7cf8dc3e2
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-19 11:02:36 +0000

    object

---
+++ 02_basics/04_objectss.js
@@ -0,0 +1,32 @@
+// const tinderuser = new Object();
+
+const tinderuser = {}
+
+tinderuser.name = "kashish";
+tinderuser.age = 21;
+tinderuser.isLoggedIn = false;
+tinderuser.lastLoginDays = ["Monday", "Friday"];
+
+
+// console.log(tinderuser);
+
+const regularUser = {
+    fullName: {
+        userName:{
+            firstName: "kashish",
+            lastName: "jat"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userName.lastName);
+
+obj1 ={1: "a", 2:"b", 6:"c"};
+obj2 ={7: "a", 8:"b", 9:"c"};
+obj3 ={3: "d", 4:"b", 5:"c"};
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3);
+
+const obj4 = {...obj1, ...obj2, ...obj3};
+console.log(obj4);
+
