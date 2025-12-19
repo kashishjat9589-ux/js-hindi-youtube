4977aa3ac0d3f25927c0da17eb89d2417f20cfe3
kashishjat9589-ux <kashishjat9589@gmail.com>
2025-12-19 09:55:00 +0000

    arrayss

---
+++ 02_basics/02_array.js
@@ -0,0 +1,28 @@
+const fruits = ['apple', 'banana', 'cherry','date','mango'];
+const vegetables = ['carrot', 'broccoli', 'spinach','potato','cabbage'];
+
+// fruits.push(vegetables);
+
+// const all_veg =vegetables.concat(fruits);
+// console.log(all_veg);
+
+// const all_veg = [...fruits,...vegetables];
+// console.log(all_veg);
+
+const num = [1,2,3,4,[2,3,4],6,7,[4,5,[7,5]]];
+// const new_num = num.flat(Infinity);
+// console.log(new_num);
+
+
+// console.log(Array.isArray("kashi"));
+// console.log(Array.from("kashi"));
+
+// console.log(Array.of(1,2,3,4,5,"kashi"));
+
+let score1=100
+let score2=200
+let score3=300
+
+const scores = Array.of(score1,score2,score3);
+console.log(scores);
+
